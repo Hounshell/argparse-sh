8f7c84de5923f7aca275bf54db109f185c3083a2
Paul Hounshell <paul@hounshell.com>
2024-02-16 08:46:22 -0800

    Add negated boolean flags.
    The help text still needs some work.

--- src/arguments/argument_boolean.rs
+++ src/arguments/argument_boolean.rs
@@ -4,36 +4,70 @@ use super::argument::Argument;
 use super::argument_common::ArgumentCommon;
 use super::argument_common::ArgumentCommonBuilder;
 use super::argument_common::MatchResult;
+use super::errors::error;
 use super::errors::OptionExt;
+use super::errors::DEFINITION_ERROR;
 use super::errors::USER_ERROR;
 
 pub struct BooleanArgument {
   common: ArgumentCommon,
+  negative_flags: Vec<String>,
 }
 
 impl BooleanArgument {
   pub fn new(args: &mut VecDeque<String>) -> Self {
     let mut common = ArgumentCommon::new_builder();
-    match common.parse_arguments(args) {
-      None => { }
-      Some(other) => {
-        args.push_front(other);
+    let mut negative_flags = Vec::new();
+
+    loop {
+      match common.parse_arguments(args).as_deref() {
+        None => {
+          break;
+        }
+        Some("--negative-flag") | Some("--negative") | Some("--neg") => {
+          negative_flags.push(args.pop_front()
+                .unwrap_or_error(DEFINITION_ERROR, String::from("flag must be provided after --negative-flag"))
+                .to_string());
+        }
+        Some(other) => {
+          args.push_front(other.to_string());
+          break;
+        }
       }
     }
 
+    let common = common.build();
+
+    if common.get_repeated() {
+      error(DEFINITION_ERROR, format!("Boolean argument {} can not be repeated", common.get_name()));
+
+    } else if common.get_catch_all() {
+      error(DEFINITION_ERROR, format!("Boolean argument {} can not be catch-all", common.get_name()));
+
+    } else if !common.get_ordinals().is_empty() {
+      error(DEFINITION_ERROR, format!("Boolean argument {} can not be ordinal", common.get_name()));
+    }
+
     return BooleanArgument {
-      common: common.build(),
+      common: common,
+      negative_flags: negative_flags,
     };
   }
 }
 
 impl Argument for BooleanArgument {
   fn get_help_flags(&self) -> Vec<String> {
-    self.common
-        .get_all_flags()
-        .iter()
-        .map(|flag| format!("{flag}[=<true|false>]"))
-        .collect()
+    [
+        self.common
+          .get_all_flags()
+          .iter()
+          .map(|flag| format!("{flag}[=<true|false>]"))
+          .collect::<Vec<String>>(),
+        self.negative_flags
+          .iter()
+          .map(|flag| format!("{flag}"))
+          .collect::<Vec<String>>()
+    ].concat()
   }
 
   fn get_help_default(&self) -> Option<String> {
@@ -50,14 +84,24 @@ impl Argument for BooleanArgument {
   }
 
   fn consume(&self, arg: Option<String>, _other_args: &mut VecDeque<String>) -> Option<String> {
-    match self.common.check_flag_match(arg) {
-      MatchResult::NoMatch => None,
-      MatchResult::MatchWithoutValue => Some(String::from("true")),
-      MatchResult::MatchWithValue(value) => Some(value
-        .parse::<bool>()
-        .unwrap_or_error(USER_ERROR, format!("Non-boolean value '{value}' provided for argument {}", self.get_name()))
-        .to_string()),
+    match self.common.check_flag_match(arg.clone()) {
+      MatchResult::NoMatch => {}
+      MatchResult::MatchWithoutValue => {
+        return Some(String::from("true"));
+      }
+      MatchResult::MatchWithValue(value) => {
+        return Some(value
+          .parse::<bool>()
+          .unwrap_or_error(USER_ERROR, format!("Non-boolean value '{value}' provided for argument {}", self.get_name()))
+          .to_string());
+       }
+    };
+
+    if self.negative_flags.contains(&arg.unwrap()) {
+      return Some(String::from("false"));
     }
+
+    return None
   }
 }
 
